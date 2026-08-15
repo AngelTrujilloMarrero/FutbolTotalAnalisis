8b02ca18383365d51407d2b442c529335dfb346c
partidos176 <partidos176@users.noreply.github.com>
2026-08-15 18:46:28 +0100

    Rename DESPEJE DEL DEFENSA button to DESPEJE DEFENSA

--- src/App.jsx
+++ src/App.jsx
@@ -368,7 +368,7 @@ export default function App() {
                       letterSpacing: '0.05em'
                     }}
                   >
-                    <span>DESPEJE DEL DEFENSA</span>
+                    <span>DESPEJE DEFENSA</span>
                     <span style={{
                       background: '#ffffff',
                       color: '#16a34a',
